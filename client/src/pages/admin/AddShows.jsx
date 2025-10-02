d7dffc25ee4e225b97d38c969f24219c1e0d02d3
Sanket Kulkarni <sanketkulkarni2005@gmail.com>
2025-10-02 23:45:39 +0530

    admin side bar started

---
+++ client/src/pages/admin/AddShows.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddShows = () => {
+    return (
+        <div>
+            <h1>Add Shows Page</h1>
+        </div>
+    );
+};
+
+export default AddShows;
---
+++ client/src/pages/admin/ListBookings.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ListBookings = () => {
+    return (
+        <div>
+            <h1>List Bookings</h1>
+        </div>
+    );
+};
+
+export default ListBookings;
