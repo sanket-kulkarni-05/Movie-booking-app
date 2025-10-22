f55ef853e21ddc45d071522bee41ab5282a5d08d
Sanket Kulkarni <sanketkulkarni2005@gmail.com>
2025-10-22 13:42:09 +0530

    inngest error fixed

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -7,43 +7,39 @@ const syncUserCreation = inngest.createFunction(
     {id: 'sync-user-creation-from-clerk'},
     {event: 'clerk/user.created'},
     async({ event }) => {
-
-        const {id, first_name, last_name, email_address, image_url}=event.data
+        const {id, first_name, last_name, email_addresses, image_url} = event.data;
         const userData = {
-            _id:id,
-            email: email_address[0].email_address,
+            _id: id,
+            email: email_addresses[0].email_address,
             name: first_name + ' ' + last_name,
             image: image_url
-        }
-        await User.create(userData)    
+        };
+        await User.create(userData);
     }
-)
-
+);
 
 const syncUserDeletion = inngest.createFunction(
     {id: 'sync-user-deletion-from-clerk'},
     {event: 'clerk/user.deleted'},
     async({ event }) => {
-
-        const {id } =event.data;
-        await User.findByIdAndDelete(id)   
+        const {id} = event.data;
+        await User.findByIdAndDelete(id);
     }
-)
-
+);
 
 const syncUserUpdation = inngest.createFunction(
     {id: 'sync-user-updation-from-clerk'},
     {event: 'clerk/user.updated'},
     async({ event }) => {
-
-        const {id, first_name, last_name, email_address, image_url}=event.data
+        const {id, first_name, last_name, email_addresses, image_url} = event.data;
         const userData = {
-            _id:id,
-            email: email_address[0].email_address,
+            _id: id,
+            email: email_addresses[0].email_address,
             name: first_name + ' ' + last_name,
             image: image_url
-        }
-        await User.findByIdAndUpdate(id, userData)
+        };
+        await User.findByIdAndUpdate(id, userData);
     }
-)
+);
+
 export const functions = [syncUserCreation, syncUserDeletion, syncUserUpdation];
